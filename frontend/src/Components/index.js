3ba9249a1180b696dd5a31007a62796dc7ed6165
shivam <upadhyayshivam06@gmail.com>
2021-07-03 00:41:23 +0530

    add : card added

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,1 +1,2 @@
-export {Navbar} from "./navbar/navbar"
+export {Navbar} from "./navbar/navbar";
+export { MovieCard } from "./movieCard/card";
--- frontend/src/Components/movieCard/card.jsx
+++ frontend/src/Components/movieCard/card.jsx
@@ -1,1 +1,30 @@
 import "./card.css";
+export const MovieCard = ({
+  posterimg,
+  title,
+  imdbratings,
+  releasedate,
+  duration,
+  cast,
+  geners,
+}) => {
+  return (
+    <section className="card-sec">
+      <div className="card-div">
+        <img className="card-img" src={posterimg} alt="" />
+        <div className="desc-div">
+          <h1 className="card-title">{title}</h1>
+          <p>IMDB ratings : {imdbratings}</p>
+          <div>
+            <p>Cast : {cast.join()} </p>
+            <p>Geners : {geners.join()}</p>
+          </div>
+          <p>Duration :{duration}</p>
+          <p className="release-p">
+            Realise Date{releasedate}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
